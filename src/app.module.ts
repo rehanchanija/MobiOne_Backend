a6d8465a632910c082a46832fbf71fffd5bdd815
Mo Rehan Chanija <rehanchanija@gmail.com>
2025-09-22 21:59:47 +0530

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [
@@ -13,7 +12,6 @@ import { ProductsModule } from './modules/products/products.module';
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/mobione'),
     AuthModule,
-    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
